c6425f94a4bf5ea342af92e0e3bd758949127351
MACGalaviz <macgalaviz@hotmail.com>
2023-03-09 01:31:56 -0600

    ⚗️ Test FrontEnd / BackEnd conection
    // Miguel Cabañas

---
+++ Frontend/src/lib/Components/node-conection.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { onMount } from "svelte";
+    const endpoint = "http://localhost:3000/";
+    let object = { data: ""}
+
+    onMount(async function () {
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        object = data
+    });
+    
+</script>
+<h1>{object.data}</h1>
